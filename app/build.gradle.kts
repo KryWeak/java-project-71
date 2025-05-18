19c2001b4e5ae4a1ad7fdd82c14579ede74cf556
KryWeak <kmoretti999@gmail.com>
2025-05-19 01:57:09 +0300

    Created 2 files with .json extension and added reading and parsing files to the App class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("info.picocli:picocli:4.7.7")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,16 @@
 package  hexlet.code;
 
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 
 @Command(
         name = "gendiff",
@@ -14,28 +20,50 @@ import picocli.CommandLine.Parameters;
 
 )
 
-public class App implements Runnable{
+public class App implements Runnable {
 
-    @CommandLine.Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
     String format = "stylish";
 
-    @CommandLine.Parameters(index = "0", paramLabel = "filepath1", description = "path to firs file")
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to firs file")
     String filepath1;
 
-    @CommandLine.Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
     String filepath2;
 
-
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new App()).execute(args);
-        System.exit(exitCode);
+
+        try {
+            int exitCode = new CommandLine(new App()).execute(args);
+            System.exit(exitCode);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
 
     @Override
     public void run() {
-        System.out.println("Please provide input files to compare.");
+        try {
+            Map<String, Object> data1 = getData(filepath1);
+            Map<String, Object> data2 = getData(filepath2);
+
+            System.out.println("File 1 contents:");
+            System.out.println(data1);
+            System.out.println("File 2 contents:");
+            System.out.println(data2);
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
+    private static Map<String, Object> getData(String filePath) throws Exception {
+        Path path = Path.of(filePath).toAbsolutePath().normalize();
+        String content = Files.readString(path);
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(content, new TypeReference<>() {});
 
+    }
 
 }
